b7e8b7bb2648b4330b74ccc2c786765e06f4255b
adammy123 <adam.sajc@gmail.com>
2018-09-27 22:50:07 +0800

    Added fake menu and back button

---
+++ Assets/Scripts/Menu_Controller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Controller : MonoBehaviour
+{
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene("Done_Main", LoadSceneMode.Single);
+    }
+
+    public void LoadFakeMenuScene(){
+        SceneManager.LoadScene("Fake_Menu", LoadSceneMode.Single);
+    }
+}
